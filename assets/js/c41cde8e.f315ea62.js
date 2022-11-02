ca3aee9d5ea6a5030f27b66873fbbd44e3600aec
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-11-02 12:23:37 +0000

    deploy: 1593b0fbfda725aa0b017e0fb8b2d57008cc5d38

---
+++ assets/js/c41cde8e.f315ea62.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[7224],{1693:e=>{e.exports=JSON.parse('{"permalink":"/cardano-updates/tags/consensus","page":1,"postsPerPage":5,"totalPages":1,"totalCount":5,"blogDescription":"Regular updates from Cardano Development Teams","blogTitle":"Cardano Development Updates"}')}}]);
