7d97028ce0d36fe2c15f4683216a82de859b5af1
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-11-02 12:55:28 +0000

    deploy: ba5ae2c45fcfe22a0845d8d4e15eb292236f80ad

--- assets/js/a152b641.d59087bf.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[4322],{9149:e=>{e.exports=JSON.parse('{"permalink":"/cardano-updates/tags/ledger","page":1,"postsPerPage":5,"totalPages":1,"totalCount":4,"blogDescription":"Regular updates from Cardano Development Teams","blogTitle":"Cardano Development Updates"}')}}]);
