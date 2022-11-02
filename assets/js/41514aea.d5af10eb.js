5e411f73cf17a5792e7939fed86eef2894f5b92b
Arnaud Bailly <arnaud.bailly@iohk.io>
2022-11-02 13:04:02 +0000

    deploy: e5698ca68a6287662ceca57523c693e9fca2694c

--- assets/js/41514aea.d5af10eb.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[5045],{6178:a=>{a.exports=JSON.parse('{"permalink":"/cardano-updates/","page":1,"postsPerPage":5,"totalPages":5,"totalCount":22,"nextPage":"/cardano-updates/page/2","blogDescription":"Regular updates from Cardano Development Teams","blogTitle":"Cardano Development Updates"}')}}]);
